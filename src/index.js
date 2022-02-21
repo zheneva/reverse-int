4786abd0c1c8a44008a93ba177faf174beb05d9a
zheneva <mr_zhenkins@mail.ru>
2022-02-21 22:32:58 +0300

    feat: implement reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    
+let mas = n.toString().split('');
+let newMas =[];
+for (let i = mas.length - 1; i >=0; i-- ){ 
+    if (mas[i] !== '-') newMas.push(mas[i]);
+}
+
+return Number(newMas.join(''));
 }
